1c70a29521b8c7a3384080728ec8b8ef6ec1adf8
Anwar Madani <15781928+AnwarMadani@users.noreply.github.com>
2024-10-28 09:22:25 +0100

    add logic to open and close modal

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const myLibrary = [];
+const addBookBtn = document.getElementById('addBookBtn');
+const addBookModal = document.getElementById('addBookModal');
+const overlay = document.getElementById('overlay');
+const booksContainer = document.getElementById('booksContainer');
+const addBookForm = document.getElementById('addBookForm');
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -19,4 +24,22 @@ function addBookToLibrary(newBook){
     if(!myLibrary.includes(newBook)){
         myLibrary.push(newBook);
     }
+}
+
+function removeBook(title){
+    myLibrary = myLibrary.filter(book => book.title !== title)
+}
+
+addBookBtn.addEventListener("click", openAddBookModal);
+overlay.addEventListener("click", closeAddBookModal);
+
+function openAddBookModal(){
+    addBookForm.reset();
+    addBookModal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeAddBookModal(){
+    addBookModal.classList.remove('active');
+    overlay.classList.remove('active');
 }
